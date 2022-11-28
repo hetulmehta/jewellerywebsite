eaf1c7082f2f7a81240930c3efab58a22c670cf7
hetulmehta <hetulmehta08@gmail.com>
2022-11-29 00:10:56 +0530

    db

--- db.php
+++ db.php
@@ -1,7 +1,8 @@
 <?php
     // Enter your host name, database username, password, and database name.
     // If you have not set database password on localhost then set empty.
-    $con = mysqli_connect("localhost:3307","root","","embelorn_db");
+    // $con = mysqli_connect("localhost:3307","root","","embelorn_db");
+    $con = mysqli_connect("remotemysql.com","ZlPXV0vWqF","p7MwpfuvQC","ZlPXV0vWqF");
     // Check connection
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
